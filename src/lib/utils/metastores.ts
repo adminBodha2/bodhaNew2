19227f210a7d72175b482da71749c104d805a9bc
adminBodha-Amrit <275160098+adminBodha2@users.noreply.github.com>
2026-05-09 20:51:33 +0530

    cleaningboy

--- src/lib/utils/metastores.ts
+++
@@ -1,7 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const metaTitle = writable('Bodha')
-export const metaDescription = writable('Cultural Think Tank')
-export const metaUrl = writable('https://www.bodharesearch.in')
-export const metaImage = writable('')
-export const metaType = writable('webpage')
